efdfa1eb0c2a5f316ccc00114c00c0c07707b239
McKinley Forbes <aragothofdagobah@gmail.com>
2017-04-14 12:53:21 -0500

    test

--- storeTweak.js
+++ storeTweak.js
@@ -1,10 +1,3 @@
 $(document).ready(function(){
-  $('.header-logo-image').mouseenter(function() {
-    $(this).css("cursor","pointer");
-    $(this).animate({width: "50%", height: "50%"}, 'slow');
-  });
-
-  $('.header-logo-image').mouseleave(function() {
-    $(this).animate({width: "28%"}, 'slow');
-  });
+  console.log("hello world");
 });
