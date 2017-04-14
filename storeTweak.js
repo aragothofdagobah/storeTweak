0c529019e53654403a3563d5ef641e45eea8a2c5
McKinley Forbes <aragothofdagobah@gmail.com>
2017-04-14 12:59:53 -0500

    added header image functionallity

--- storeTweak.js
+++ storeTweak.js
@@ -1,5 +1,8 @@
-$(document).ready(function(){
-  $('.header-logo-image').mouseenter(function() {
-    console.log("hello world");
-  });
+$('.header-logo-image').mouseenter(function() {
+  $(this).css("cursor","pointer");
+  $(this).animate({width: "50%", height: "50%"}, 'slow');
+});
+
+$('.header-logo-image').mouseleave(function() {
+  $(this).animate({width: "28%"}, 'slow');
 });
